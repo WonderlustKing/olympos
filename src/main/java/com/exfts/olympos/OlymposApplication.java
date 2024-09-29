26aef3e7198d707b4a348ae319970766563024a1
christos.banatsas <chrisban4@gmail.com>
2024-09-29 17:46:52 +0300

    Fix project structure

---
+++ src/main/java/com/exfts/olympos/OlymposApplication.java
@@ -0,0 +1,13 @@
+package com.exfts.olympos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlymposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlymposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exfts/olympos/OlymposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exfts.olympos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlymposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
